87b67717a9a18ba5f2745f22f7d7a132df40fe69
Francesco Piantini <francesco.piantini@gmail.com>
2020-11-24 00:13:42 +0100

    first (over)implementation

--- include/cmdsuzdal/bitboard.h
+++ include/cmdsuzdal/bitboard.h
@@ -8,9 +8,41 @@ namespace cSzd
 
   typedef uint64_t BitBoardState;
 
+  // Bitboard important definitions
+  const BitBoardState EmptyBB = 0x0000000000000000ULL;
+
+  typedef enum cellname_e {
+    a1, b1, c1, d1, e1, f1, g1, h1,
+    a2, b2, c2, d2, e2, f2, g2, h2,
+    a3, b3, c3, d3, e3, f3, g3, h3,
+    a4, b4, c4, d4, e4, f4, g4, h4,
+    a5, b5, c5, d5, e5, f5, g5, h5,
+    a6, b6, c6, d6, e6, f6, g6, h6,
+    a7, b7, c7, d7, e7, f7, g7, h7,
+    a8, b8, c8, d8, e8, f8, g8, h8
+  } Cell;
+
+  typedef enum filename_e {
+    f_a, f_b, f_c, f_d, f_e, f_f, f_g, f_h
+  } File;
+  typedef enum rankname_e {
+    r_1, r_2, r_3, r_4, r_5, r_6, r_7, r_8
+  } Rank;
+
   // base class for bitboard representation
   class BitBoard
   {
+    public:
+      BitBoard() { bbs = EmptyBB; }
+
+      BitBoardState state() const { return bbs; }
+      BitBoardState setCell(File f, Rank r) { return (bbs |= 1ULL << (r * 8 + f)); }
+      BitBoardState resetCell(File f, Rank r) { return (bbs &= ~(1ULL << (r *8 + f))); }
+      BitBoardState setCellByName(Cell c) { return (bbs |= 1ULL << c); }
+      BitBoardState resetCellByName(Cell c) { return (bbs &= ~(1ULL << c)); }
+
+    private:
+      BitBoardState bbs;
   };
 
 } // namespace cSzd
--- test/bitboardtest.cpp
+++ test/bitboardtest.cpp
@@ -6,8 +6,37 @@ using namespace std;
 using namespace cSzd;
 using namespace testing;
 
-class BBTester : public Test
+// Bitboard not important definitions (just for tests)
+const BitBoardState A1ActiveBB = 0x0000000000000001ULL;
+
+// --------------------------------------------------------
+TEST(BBTester, AfterDefinitionBitboardIsEmpty)
 {
-public:
-    BitBoard bboard;
-};
+    BitBoard bb;
+    ASSERT_EQ(bb.state(), EmptyBB);
+}
+
+TEST(BBTester, Cell_0_0_CorrespondToA1)
+{
+    BitBoard bb;
+    ASSERT_EQ(bb.setCell(f_a, r_1), 1ULL << a1);
+}
+
+TEST(BBTester, IfACellIsSetAndThenResetBitboardIsEmpty)
+{
+    BitBoard bb;
+    bb.setCell(f_a, r_1);
+    ASSERT_EQ(bb.resetCell(f_a, r_1), EmptyBB);
+}
+
+TEST(BBTester, Cell_3_7_CorrespondToH4)
+{
+    BitBoard bb;
+    ASSERT_EQ(bb.setCell(f_h, r_4), 1ULL << h4);
+}
+
+TEST(BBTester, CheckCellSetByName)
+{
+    BitBoard bb;
+    ASSERT_EQ(bb.setCellByName(c4), 1ULL << (f_c + r_4 * 8));
+}
