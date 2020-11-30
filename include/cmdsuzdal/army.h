c24043c48513d67a8b30242b1b655198c4ae16ea
Francesco Piantini <francesco.piantini@gmail.com>
2020-11-30 23:35:09 +0100

    refactoring: added a base class for the 'army items' (pieces and pawns sets); declared King and PawnsSet as derived

--- include/cmdsuzdal/army.h
+++ include/cmdsuzdal/army.h
@@ -8,30 +8,42 @@
 namespace cSzd
 {
 
-    // --- The King ---------------------------------
-    struct King {
+    // --- The Base Item (piece or pawn set) --------
+    // This class is used as abstract base class
+    // for all piece and piecesets
+    struct ItemsSet {
+        ItemsSet() = default;
+        ItemsSet(const Cell &c) : bb(c) {};
+        ItemsSet(const std::vector<Cell> &c) : bb(c) {};
 
-        King() = default;
-        King(Cell c) : bb(c) {};
-
-        bool valid() { return bb.popCount() == 1; }
+        virtual bool valid() const = 0;
 
         // the bitboard
         BitBoard bb {};
     };
     // -----------------------------------------------
 
+
+    // --- The King ---------------------------------
+    struct King : ItemsSet {
+
+        King() = default;
+        King(Cell c) : ItemsSet(c) {};
+
+        bool valid() const { return bb.popCount() == 1; }
+
+    };
+    // -----------------------------------------------
+
     // --- The Pawns Set -----------------------------
-    struct PawnSet {
+    struct PawnsSet : ItemsSet {
 
-        PawnSet() = default;
-        PawnSet(const Cell &c) : bb(c) {};
-        PawnSet(const std::vector<Cell> &c) : bb(c) {};
+        PawnsSet() = default;
+        PawnsSet(const Cell &c) : ItemsSet(c) {};
+        PawnsSet(const std::vector<Cell> &c) : ItemsSet(c) {};
 
-        bool valid() { return !bb.activeCellsInMask(RanksBB[r_1] | RanksBB[r_8]); }
+        bool valid() const { return !bb.activeCellsInMask(RanksBB[r_1] | RanksBB[r_8]); }
 
-        // the bitboard
-        BitBoard bb {};
     };
     // -----------------------------------------------
 
--- test/armytest.cpp
+++ test/armytest.cpp
@@ -23,32 +23,32 @@ namespace cSzd
     // ---- Pawn set ---------------------------------------------------
     TEST(ArmyTester, PositionIsValidIfNoPawns)
     {
-        PawnSet pSet;
+        PawnsSet pSet;
         ASSERT_TRUE(pSet.valid());
     }
     TEST(ArmyTester, PositionIsValidIfOnePawnInA2)
     {
-        PawnSet pSet {a2};
+        PawnsSet pSet {a2};
         ASSERT_TRUE(pSet.valid());
     }
     TEST(ArmyTester, PositionIsNotValidIfOnePawnInRank1)
     {
-        PawnSet pSet {b1};
+        PawnsSet pSet {b1};
         ASSERT_FALSE(pSet.valid());
     }
     TEST(ArmyTester, PositionIsNotValidIfOnePawnInRank8)
     {
-        PawnSet pSet {e8};
+        PawnsSet pSet {e8};
         ASSERT_FALSE(pSet.valid());
     }
     TEST(ArmyTester, PositionIsNotValidIfOnePawnInRank8WithOtherCorrectPawns)
     {
-        PawnSet pSet {{a2, d4, f6, g8}};
+        PawnsSet pSet {{a2, d4, f6, g8}};
         ASSERT_FALSE(pSet.valid());
     }
     TEST(ArmyTester, PositionIsValidIfEightPawnsNotInBoundaryRanks)
     {
-        PawnSet pSet {{a2, b3, b5, d4, e5, f6, g2}};
+        PawnsSet pSet {{a2, b3, b5, d4, e5, f6, g2}};
         ASSERT_TRUE(pSet.valid());
     }
 
