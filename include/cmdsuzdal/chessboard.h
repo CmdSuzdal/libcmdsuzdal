b1307e04039dba671bd0f10db6ea2111b19fac67
Francesco Piantini <francesco.piantini@gmail.com>
2020-12-07 14:25:16 +0100

    ChessBoard: default constructor

--- include/cmdsuzdal/chessboard.h
+++ include/cmdsuzdal/chessboard.h
@@ -7,8 +7,47 @@
 namespace cSzd
 {
 
+    // ------------------------------------------------------------------------
+    // ChessBoard is a class used to represent a valid chess position based on
+    // usage of BitBoards. For the Armies rapresentation, the BitBoard-based
+    // Army class is used. Other game information are also stored (used the FEN
+    // format as a reference)
+    //
+    // ChessBoard
+    //   |
+    //   ├─ Army whiteArmy;
+    //   |    └─ BitBoard pieces[NumPieceTypes]
+    //   |       ├─ pieces[King]  : BitBoard with the position of the white king
+    //   |       ├─ pieces[Queen] : BitBoard with the position of the white queen(s)
+    //   |       ├─ pieces[Bishop]: BitBoard with the position of the white bishop(s)
+    //   |       ├─ pieces[Knight]: BitBoard with the position of the white knight(s)
+    //   |       ├─ pieces[Rook]  : BitBoard with the position of the white rook(s)
+    //   |       └─ pieces[Pawn]  : BitBoard with the position of the white pawn(s)
+    //   ├─ Army blackArmy;
+    //   |    └─ BitBoard pieces[NumPieceTypes]
+    //   |       ├─ pieces[King]  : BitBoard with the position of the black king
+    //   |       ├─ pieces[Queen] : BitBoard with the position of the black queen(s)
+    //   |       ├─ pieces[Bishop]: BitBoard with the position of the black bishop(s)
+    //   |       ├─ pieces[Knight]: BitBoard with the position of the black knight(s)
+    //   |       ├─ pieces[Rook]  : BitBoard with the position of the black rook(s)
+    //   |       └─ pieces[Pawn]  : BitBoard with the position of the black pawn(s)
+    //   ├─ BitBoard castlingAvail;
+    //   ├─ BitBoard enPassantMove;
+    //   ├─ ArmyColor sideToMove;
+    //   ├─ unsigned int halfMoveClock;
+    //   └─ unsigned int fullMoves;
+    //
+    // ------------------------------------------------------------------------
+
     // --- The ChessBoard -----------------------------
     struct ChessBoard {
+        // --------------------------
+        Army whiteArmy = Army(WhiteArmy);
+        Army blackArmy = Army(BlackArmy);
+        ArmyColor sideToMove = WhiteArmy;
+        unsigned int halfMoveClock = 0;
+        unsigned int fullMoves = 1;
+
         // --------------------------
         explicit ChessBoard() = default;
 
--- test/chessboardtest.cpp
+++ test/chessboardtest.cpp
@@ -8,8 +8,25 @@ using namespace testing;
 namespace cSzd
 {
 
-    TEST(ChessBoardTester, DummyTest)
+    TEST(ChessBoardTester, DefaultConstructorPrepareInitialStandardPosition)
     {
+        ChessBoard cb {};
+        ASSERT_EQ(cb.whiteArmy.pieces[King], BitBoard({e1}));
+        ASSERT_EQ(cb.whiteArmy.pieces[Queen], BitBoard({d1}));
+        ASSERT_EQ(cb.whiteArmy.pieces[Rook], BitBoard({a1, h1}));
+        ASSERT_EQ(cb.whiteArmy.pieces[Knight], BitBoard({b1, g1}));
+        ASSERT_EQ(cb.whiteArmy.pieces[Bishop], BitBoard({c1, f1}));
+        ASSERT_EQ(cb.whiteArmy.pieces[Pawn], BitBoard(RanksBB[r_2]));
+        ASSERT_EQ(cb.blackArmy.pieces[King], BitBoard({e8}));
+        ASSERT_EQ(cb.blackArmy.pieces[Queen], BitBoard({d8}));
+        ASSERT_EQ(cb.blackArmy.pieces[Rook], BitBoard({a8, h8}));
+        ASSERT_EQ(cb.blackArmy.pieces[Knight], BitBoard({b8, g8}));
+        ASSERT_EQ(cb.blackArmy.pieces[Bishop], BitBoard({c8, f8}));
+        ASSERT_EQ(cb.blackArmy.pieces[Pawn], BitBoard(RanksBB[r_7]));
+        ASSERT_EQ(cb.sideToMove, WhiteArmy);
+        ASSERT_EQ(cb.halfMoveClock, 0);
+        ASSERT_EQ(cb.fullMoves, 1);
+
     }
 
 } // namespace cSzd
